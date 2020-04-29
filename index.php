a92455bbc9dcdb90137694ffc7fd8b64b22d17d1
IBeThieving <35572416+IBeThieving@users.noreply.github.com>
2020-04-29 20:58:41 +0100

    Minor changes

--- index.php
+++ index.php
@@ -2,11 +2,11 @@
 
 // Include the class.
 
-include_once './objects/SecurePassword.class.php';
+include_once './objects/BcryptPassword.class.php';
 
 // Initialize an instance of the class.
 
-$password_secure = new SecurePassword();
+$bcrypt_password = new BcryptPassword();
 
 // Specifiy the lenght of the salt note that bcrypt expects atleast 22 characters.
 
@@ -14,11 +14,11 @@ $salt_lenght = 22;
 
 // Get password salt.
 
-$salt = $password_secure->GetSalt($salt_lenght);
+$salt = $bcrypt_password->GetSalt($salt_lenght);
 
 // Get password hash passing the salt as parameter.
 
-$password = $password_secure->GetHash('password123', $salt);
+$password = $bcrypt_password->GetHash('password123', $salt);
 
 // Example output.
 
